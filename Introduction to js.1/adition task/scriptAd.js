fbbf5c9b0abdf23e2a3748c463fcf79757423b13
MykolaTsar <mykola.tsar@gmail.com>
2023-03-11 12:58:37 +0200

    aded new resolving of adding task

--- Introduction to js.1/adition task/scriptAd.js
+++ Introduction to js.1/adition task/scriptAd.js
@@ -5,15 +5,14 @@
 const ticket = prompt('Entered number of Your ticket:', '');
 // console.log(ticket);
 // console.log(typeof ticket);
-const ticketNum = parseInt(ticket);
+// const ticketNum = ticket;
 // console.log(ticketNum);
 // console.log(typeof ticketNum);
 
-const coupeNumber = ticketNum / 9;
+const coupeNumber = ticket / 9;
 const enteredCoupeNumber = Math.ceil(coupeNumber);
 
-
-if (ticketNum < 1 || ticketNum > 54 || isNaN(ticketNum)) {
+/* if (ticketNum < 1 || ticketNum > 54 || isNaN(ticketNum)) {
     alert('Invalid tickets number!');
 } else if (ticketNum % 6 === 1 || ticketNum % 6 === 3) {
     alert(`Your place is in coupe №${enteredCoupeNumber}, down-coupe.`);
@@ -23,4 +22,31 @@ if (ticketNum < 1 || ticketNum > 54 || isNaN(ticketNum)) {
     alert(`Your place is in coupe №${enteredCoupeNumber}, down-side.`);
 } else if (ticketNum % 6 === 0) {
     alert(`Your place is in coupe №${enteredCoupeNumber}, up-side.`);
+} */
+let findPlace;
+
+ticket <= 54 ? findPlace = (ticket % 6) : '';
+
+
+switch (findPlace) {
+  case 1:
+    alert(`Your place in the coupe №${enteredCoupeNumber}, down-coupe`);
+    break;
+  case 3:
+    alert(`Your place in the coupe №${enteredCoupeNumber}, down-coupe`);
+    break;
+  case 2:
+    alert(`Your place in the coupe №${enteredCoupeNumber}, up-coupe`);
+    break;
+  case 4:
+    alert(`Your place in the coupe №${enteredCoupeNumber}, up-coupe`);
+    break;
+  case 5:
+    alert(`Your place in the coupe №${enteredCoupeNumber}, down-side`);
+    break;
+  case 0:
+    alert(`Your place in the coupe №${enteredCoupeNumber}, up-side`);
+    break;
+  default:
+    alert(`Invalid tickets number!`);
 }
