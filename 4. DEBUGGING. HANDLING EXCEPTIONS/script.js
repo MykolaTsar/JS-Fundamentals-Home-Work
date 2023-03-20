15c9429ae415478569f7d7640db8308d18a9a1fb
Mykola Tsar <Mykola.tsar@gmail.com>
2023-03-20 12:28:41 +0200

    added last exercise of block 4

--- 4. DEBUGGING. HANDLING EXCEPTIONS/script.js
+++ 4. DEBUGGING. HANDLING EXCEPTIONS/script.js
@@ -1,6 +1,7 @@
+// ДЛЯ ЗРУЧНОСТІ ПЕРЕВІРКИ, РОЗКОМЕНТОВУВАТИ ТІЛЬКИ БЛОЧНІ КОМЕНТАРІ!/*  */
 // 1.Напишіть функцію sumSliceArray(arr, first, second), яка приймає масив arr і два числа (first и second) – порядкові номери елементів масиву, які необхідно скласти. Наприклад, якщо ввели 3 та 5 – сумуються 3-й та 5-й елементи.Функція повинна генерувати винятки, якщо були введені не числа, і коли одне з чисел або обидва більшого розміруза довжину масиву. Напишіть код, який використовує цю функцію, передбачте обробку можливих винятків.
 
-/* const sumSliceArray = (arr, first, second) => {
+const sumSliceArray = (arr, first, second) => {
   try {
     first = parseInt(first);
     second = parseInt(second);
@@ -17,7 +18,7 @@
 }
 
 const myArr = [1, 3, 5, 7, 8, 9];
-console.log(sumSliceArray(myArr, 2, 'f')); */
+console.log(sumSliceArray(myArr, 2, 'f'));
 
 // 2.Створіть функцію checkAge(), яка запитує у користувача його ім'я, вік та статус (адмін, модератор, користувач) та генерує модальне вікно з помилкою, якщо:вік користувача менше 18 або більше 70 років (генерується помилка типу RangeError).користувач не ввів жодних даних в будь-якому полі (виводиться повідомлення The field is empty! Please enter your age з типом помилки Error). У полі статус введено неправильне слово (тип помилки EvalError).в полі вік введено нечислове значення. У всіх інших випадках користувач отримає доступ до перегляду фільму. У блоці catch передбачена можливість виведення назви та опису помилки.
 
@@ -67,7 +68,7 @@ console.log(calcRectangleArea(2, 'a')); */
 
 // 4.Створіть клас MonthException, конструктор якого приймає параметр message і ініціалізує поле name значенням MonthException.Реалізуйте функцію showMonthName(month), в якій параметр month – це порядковий номер місяця в році. Функція повертає назву місяця відповідно до введеного номера місяця. У випадку некоректного вводу кидається ексепшн у вигляді об’єкта класу MonthException з повідомленням Incorrect month number.Напишіть код, який використовує цю функцію, передбачте обробку можливих винятків.
 
-class MonthException extends Error {
+/* class MonthException extends Error {
   constructor(message) {
     super(message);
     this.name = "MonthException";
@@ -89,7 +90,27 @@ const showMonthName = (month) => {
   }
 }
 
-console.log(showMonthName('d'));
+console.log(showMonthName('d')); */
 
 // 5.Реалізуйте функцію showUser(id), яка приймає параметром користувацьке id і повертає об’єкт, який містить значення переданої id. Також функція викидає помилку у разі якщо введено від’ємне id.Реалізуйте функцію showUsers(ids), яка приймає параметром масив користувацьких айді ids, перевіряє з використанням функції showUser() кожен елемент масиву ids на коректність, в разі виключної ситуації виводить повідомлення про помилку. Функція showUsers(ids) повертає масив об’єктів, де значеннями ключів є коректні елементи ids.
 
+/* const showUser = (id) => {
+  if (id < 0) {
+    throw Error('Число має бути більшим за нуль.')
+  }
+  return {id: id};
+}
+
+const showUsers = (ids) => {
+  const elements = [];
+  for (let i = 0; i  < ids.length; i++) {
+    try {
+      elements.push(showUser(ids[i]));
+    } catch(error) {
+      console.log(`${error.name}: ${error.message}`);
+    }
+  }
+  return elements;
+}
+
+console.log(showUsers([2, 4, -5])); */
